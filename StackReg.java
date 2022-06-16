bb7a892f059e84e9d856a42519fe6bc9778f9cb2
wu3412 <wu.3412@buckeyemail.osu.edu>
2022-06-16 12:28:35 -0400

    fixed stack checking errors

--- StackReg.java
+++ StackReg.java
@@ -10,26 +10,36 @@ class StackReg {
     static public void searchAndReplaceConst(String key, Integer value) {
         Stack<Map<String, Integer>> copyOfStack = new Stack<>();
         copyOfStack.addAll(localVar);
-        Map<String, Integer> m = copyOfStack.pop();
-        while (!m.containsKey(key)) {
-            m = copyOfStack.pop();
-        }
-        Map<String, Integer> newMap = new HashMap<>() {
-            {
-                put(key, value);
+        if (!copyOfStack.isEmpty()) {
+            Map<String, Integer> m = copyOfStack.pop();
+            while (!m.containsKey(key) && !copyOfStack.isEmpty()) {
+                m = copyOfStack.pop();
             }
-        };
-        localVar.set(localVar.indexOf(m), newMap);
+            Map<String, Integer> newMap = new HashMap<>() {
+                {
+                    put(key, value);
+                }
+            };
+            localVar.set(localVar.indexOf(m), newMap);
+        }
     }
 
     // Searches stack for given key and returns the value
+    // Returns -1 if key not found
     static public int search(String key) {
+        int returnInt = -1;
         Stack<Map<String, Integer>> copyOfStack = new Stack<>();
         copyOfStack.addAll(localVar);
-        Map<String, Integer> m = copyOfStack.pop();
-        while (!m.containsKey(key)) {
+        Map<String, Integer> m = new HashMap<>();
+        if (!copyOfStack.isEmpty()) {
             m = copyOfStack.pop();
+            while (!m.containsKey(key) && !copyOfStack.isEmpty()) {
+                m = copyOfStack.pop();
+            }
+            if (m.containsKey(key)) {
+                returnInt = m.get(key);
+            }
         }
-        return m.get(key);
+        return returnInt;
     }
 }
